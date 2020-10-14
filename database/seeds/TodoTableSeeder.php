a493a56643e4f9384a5f73106d442962ae9a87f0
2019 DSP6101 <2019DSP6101@ntsv6101.com>
2020-10-14 12:17:04 +0900

    todo表示まで

---
+++ database/seeds/TodoTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TodoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i <= 100; $i++){
+            DB::table('todos')->insert([
+                'title' => "タスク$i",
+                'due_date' => date('Y-m-d'),
+                'status' => 0,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+    }
+}
+?>
